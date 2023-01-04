07ccd51952dd900d7c577d7c6ed82ef36668c8f5
shubham pandit <shubham.sharma@antiersolutions.com>
2023-01-04 15:21:07 +0530

    swap designs are in progress and select design is done

--- src/Pages/Swap/Swap.jsx
+++ src/Pages/Swap/Swap.jsx
@@ -7,7 +7,11 @@ import BTC from "../../Assets/Coins/BTC.png";
 import DownArrowSuffix from "../../Assets/DownArrowSuffix.svg";
 import ETH from "../../Assets/Coins/ETH.png";
 import ADA from "../../Assets/Coins/ADA.png";
+import SwapVerticalIcon from '../../Assets/SwapVerticalIcon.svg'
+import ButtonComp from "../../Components/ButtonComp/ButtonComp";
+import { useNavigate } from "react-router-dom";
 function Swap() {
+    const navigate = useNavigate();
   const selectAfter = (
     <div className={"addonAfter"}>
       <span className={style.swap__pasteText}>Max</span>
@@ -58,6 +62,55 @@ function Swap() {
       />
     </div>
   );
+  const selectAfterTo = (
+    <div className={"addonAfter"}>
+    <Select
+    suffixIcon={<img src={DownArrowSuffix} />}
+    defaultValue={[
+      {
+        value: (
+          <span className="flexedItemSelect">
+            <img src={BTC} />
+            BTC
+          </span>
+        ),
+      },
+    ]}
+    style={{
+      width: 100,
+    }}
+    options={[
+      {
+        value: "BTC",
+        label: (
+          <span className="flexedItemSelect">
+            <img src={BTC} />
+            BTC
+          </span>
+        ),
+      },
+      {
+        value: "ETH",
+        label: (
+          <span className="flexedItemSelect">
+            <img src={ETH} />
+            ETH
+          </span>
+        ),
+      },
+      {
+        value: "ADA",
+        label: (
+          <span className="flexedItemSelect">
+            <img src={ADA} />
+            BTC
+          </span>
+        ),
+      },
+    ]}
+  />
+  </div>
+  )
   return (
     <div className={`scrollableCont`}>
       <MenuRestofHeaders backTo={"/wallet"} title={"Swap"} />
@@ -71,6 +124,22 @@ function Swap() {
             addonAfter={selectAfter}
           />
           <div className={style.swap__infoText}>0 BTC ~ $88.02</div>
+          <div className={style.swap__icon}>
+            <img src={SwapVerticalIcon} width={44} height={44} />
+          </div>
+          <InputField
+            mb0={true}
+            inputSelect={true}
+            placeholder={"283.00"}
+            label="To"
+            addonAfter={selectAfterTo}
+          />
+         <div style={{marginTop:"50px"}}> <ButtonComp
+          onClick={() => navigate("#")}
+          text={"Swap"}
+          maxWidth={"100%"}
+        />
+        </div>
         </div>
       </div>
     </div>
