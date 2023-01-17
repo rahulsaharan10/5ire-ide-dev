15fde68b549754bb5a94e7ae3719235a324482aa
harshveer <harshveer.singh@antiersolutions.com>
2023-01-17 18:37:30 +0530

    send and swap page design and popup design

--- src/Pages/Swap/Swap.jsx
+++ src/Pages/Swap/Swap.jsx
@@ -1,149 +1,108 @@
-import { Select } from "antd";
-import React from "react";
-import MenuRestofHeaders from "../../Components/BalanceDetails/MenuRestofHeaders/MenuRestofHeaders";
-import { InputField } from "../../Components/InputField/InputFieldSimple";
+import React, { useState } from "react";
 import style from "./style.module.scss";
-import BTC from "../../Assets/Coins/BTC.png";
-import DownArrowSuffix from "../../Assets/DownArrowSuffix.svg";
-import ETH from "../../Assets/Coins/ETH.png";
-import ADA from "../../Assets/Coins/ADA.png";
-import SwapVerticalIcon from "../../Assets/SwapVerticalIcon.svg";
-import ButtonComp from "../../Components/ButtonComp/ButtonComp";
-import { useNavigate } from "react-router-dom";
+import SwapIcon from "../../Assets/SwapIcon.svg";
+import CopyIcon from "../../Assets/CopyIcon.svg";
+import WalletCardLogo from "../../Assets/walletcardLogo.svg";
+import { InputField } from "../../Components/InputField/InputFieldSimple";
+import Approve from "../Approve/Approve";
 function Swap() {
-  const navigate = useNavigate();
-  const selectAfter = (
-    <div className={"addonAfter"}>
-      <span className={style.swap__pasteText}>Max</span>
-      <Select
-        suffixIcon={<img src={DownArrowSuffix} />}
-        defaultValue={[
-          {
-            value: (
-              <span className="flexedItemSelect">
-                <img src={BTC} />
-                BTC
-              </span>
-            ),
-          },
-        ]}
-        style={{
-          width: 100,
-        }}
-        options={[
-          {
-            value: "BTC",
-            label: (
-              <span className="flexedItemSelect">
-                <img src={BTC} />
-                BTC
-              </span>
-            ),
-          },
-          {
-            value: "ETH",
-            label: (
-              <span className="flexedItemSelect">
-                <img src={ETH} />
-                ETH
-              </span>
-            ),
-          },
-          {
-            value: "ADA",
-            label: (
-              <span className="flexedItemSelect">
-                <img src={ADA} />
-                BTC
-              </span>
-            ),
-          },
-        ]}
-      />
-    </div>
-  );
-  const selectAfterTo = (
-    <div className={"addonAfter"}>
-      <Select
-        suffixIcon={<img src={DownArrowSuffix} />}
-        defaultValue={[
-          {
-            value: (
-              <span className="flexedItemSelect">
-                <img src={BTC} />
-                BTC
-              </span>
-            ),
-          },
-        ]}
-        style={{
-          width: 100,
-        }}
-        options={[
-          {
-            value: "BTC",
-            label: (
-              <span className="flexedItemSelect">
-                <img src={BTC} />
-                BTC
-              </span>
-            ),
-          },
-          {
-            value: "ETH",
-            label: (
-              <span className="flexedItemSelect">
-                <img src={ETH} />
-                ETH
-              </span>
-            ),
-          },
-          {
-            value: "ADA",
-            label: (
-              <span className="flexedItemSelect">
-                <img src={ADA} />
-                BTC
-              </span>
-            ),
-          },
-        ]}
-      />
-    </div>
-  );
+  const [activeTab, setActiveTab] = useState("one");
+  const activeIst = () => {
+    setActiveTab("one");
+  };
+  const activeSecond = () => {
+    setActiveTab("two");
+  };
+  const activeThree = () => {
+    setActiveTab("three");
+  };
+  const activeFour = () => {
+    setActiveTab("four");
+  };
   return (
-    <div className={`scrollableCont`}>
-      <MenuRestofHeaders backTo={"/wallet"} title={"Swap"} />
-      <div className={`flexedContent`}>
-        <div className={style.swap}>
-          <InputField
-            mb0={true}
-            inputSelect={true}
-            placeholder={"Wallet Address"}
-            label="Address"
-            addonAfter={selectAfter}
-          />
-          <div className={style.swap__infoText}>0 BTC ~ $88.02</div>
-          <div className={style.swap__icon}>
-            <img src={SwapVerticalIcon} width={44} height={44} />
-          </div>
-          <InputField
-            mb0={true}
-            inputSelect={true}
-            placeholder={"283.00"}
-            label="To"
-            addonAfter={selectAfterTo}
-          />
-          <div style={{ marginTop: "50px" }}>
-            <ButtonComp
-              onClick={() => navigate("/swapDetails")}
-              text={"Swap"}
-              maxWidth={"100%"}
-            />
-          </div>
+    <>
+    <div className={style.swap}>
+      <div className={style.swap__swapCopy}>
+        <div className={style.swap__swapSec}>
+          <h3>From Native</h3>
+          <p>300 5ire</p>
+          <span>
+            0xxx0...lsh223 <img src={CopyIcon} width={11} height={11} />
+          </span>
+        </div>
+        <div className={style.swap__icon}>
+          <img src={SwapIcon} />
+        </div>
+        <div className={style.swap__swapSec}>
+          <h3>From Native</h3>
+          <p>300 5ire</p>
+          <span>
+            0xx ...lsh223 <img src={CopyIcon} width={11} height={11} />
+          </span>
         </div>
       </div>
+      <div className={style.swap__swapAccount}>
+        <div>
+          <InputField
+            placeholder={"Enter Swap Amount "}
+            addonAfter={
+              <span className={style.swap__pasteText}>
+                <img src={WalletCardLogo} />
+                5ire
+              </span>
+            }
+          />
+          <span className={style.swap__spanbalanceText}>
+            Balance 00.0000 5IRE
+          </span>
+        </div>
+        <div className={style.swap__activeBalnce}>
+          <button
+            onClick={activeIst}
+            className={`${style.swap__activeBalanceSelect__buttons} 
+              ${
+                activeTab === "one" &&
+                style.swap__activeBalanceSelect__buttons__active
+              }
+            `}
+          >
+            25 %
+          </button>
+          <button
+            onClick={activeSecond}
+            className={`${style.swap__activeBalanceSelect__buttons}  ${
+              activeTab === "two" &&
+              style.swap__activeBalanceSelect__buttons__active
+            }`}
+          >
+            50 %
+          </button>
+          <button
+            onClick={activeThree}
+            className={`${style.swap__activeBalanceSelect__buttons}  ${
+              activeTab === "three" &&
+              style.swap__activeBalanceSelect__buttons__active
+            }`}
+          >
+            70 %
+          </button>
+          <button
+            onClick={activeFour}
+            className={`${style.swap__activeBalanceSelect__buttons}  ${
+              activeTab === "four" &&
+              style.swap__activeBalanceSelect__buttons__active
+            }`}
+          >
+            100 %
+          </button>
+        </div>
+      </div>
+      <div className={style.swap__transactionFee}>
+        <p>Transaction Fee : 0.0002 5IRE</p>
+      </div>
     </div>
+    <Approve/>
+    </>
   );
 }
 
